989909d86b0e283d78c3825c37f820393f2f2ead
lukaszrzasa <luki486@interia.pl>
2019-06-09 19:44:51 +0200

    add icon library and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from "./components/includes/Navigation";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faArrowCircleLeft,
+    faCog,
+    faMinusCircle,
+    faSearch,
+    faSearchPlus,
+    faSpinner
+} from '@fortawesome/free-solid-svg-icons'
+import CitiesList from "./components/pages/CitiesList";
+import CityDetails from "./components/pages/CityDetails";
+import {Route, Switch} from "react-router-dom";
+import Settings from "./components/pages/Settings";
+
+library.add(faCog,faSearch,faSearchPlus,faMinusCircle,faArrowCircleLeft,faSpinner);
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <Navigation/>
+            <Switch>
+                <Route path="/" exact component={CitiesList} />
+                <Route path="/settings" component={Settings} />
+                <Route path="/details/:id" component={CityDetails} />
+            </Switch>
+        </div>
+    );
+};
+
 
 export default App;
